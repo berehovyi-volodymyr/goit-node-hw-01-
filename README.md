21539b2cefce614dea9cf0e8ffaa41adb8f935ba
berehovyi-volodymyr <106471456+berehovyi-volodymyr@users.noreply.github.com>
2023-03-26 16:48:58 +0300

    add screenshot

---
+++ README.md
@@ -0,0 +1,1 @@
+https://ibb.co/SN3L8gX
--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const invokeAction = async ({ action, id, name, email, phone }) => {
   switch (action) {
     case "list":
       const allContacts = await contacts.listContacts();
-      console.log(allContacts);
+      console.table(allContacts);
       break;
 
     case "get":
